78c83f701daacca994d594a9cbe07da1ea174646
Vineet <koppalkarvineet@gmail.com>
2020-08-01 15:13:26 -0400

    Updates

---
+++ precache-manifest.6efee251639b0ce48f44aa1763ef7740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adf86ad45fe21bb157d4d45cc72cb01",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "ceb00a75ad7dc50e401a",
+    "url": "/firegram/static/css/main.b3f38798.chunk.css"
+  },
+  {
+    "revision": "47009bb72e3a16741577",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb00a75ad7dc50e401a",
+    "url": "/firegram/static/js/main.adb1d72d.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
